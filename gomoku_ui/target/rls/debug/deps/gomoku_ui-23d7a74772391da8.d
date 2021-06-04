f31c95799c15790d8d5f9a524c1d3133b9364faf
trilq142 <trilq1402@gmail.com>
2021-06-03 21:51:37 -0700

    Delete gomoku_ui/target directory

--- gomoku_ui/target/rls/debug/deps/gomoku_ui-23d7a74772391da8.d
+++
@@ -1,5 +0,0 @@
-c:\Users\nguye\Desktop\Project-Caro\gomoku_ui\target\rls\debug\deps\gomoku_ui-23d7a74772391da8.rmeta: src\main.rs
-
-c:\Users\nguye\Desktop\Project-Caro\gomoku_ui\target\rls\debug\deps\gomoku_ui-23d7a74772391da8.d: src\main.rs
-
-src\main.rs:
--- gomoku_ui/target/rls/debug/deps/gomoku_ui-477a359c3cb84cc1.d
+++
@@ -1,5 +0,0 @@
-c:\Users\nguye\Desktop\Project-Caro\gomoku_ui\target\rls\debug\deps\gomoku_ui-477a359c3cb84cc1.rmeta: src\main.rs
-
-c:\Users\nguye\Desktop\Project-Caro\gomoku_ui\target\rls\debug\deps\gomoku_ui-477a359c3cb84cc1.d: src\main.rs
-
-src\main.rs:
