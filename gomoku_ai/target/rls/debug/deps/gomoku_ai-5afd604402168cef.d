32dcf3900c53957841ccd7712fa16d2bbc82f8d3
Tuan Nguyen <nguyenvutuan1@gmail.com>
2021-06-03 21:06:37 -0700

    create tests folder and add test_win.rs file into the tests folder

---
+++ gomoku_ai/target/rls/debug/deps/gomoku_ai-5afd604402168cef.d
@@ -0,0 +1,5 @@
+c:\Users\nguye\Desktop\Project-Caro\gomoku_ai\target\rls\debug\deps\gomoku_ai-5afd604402168cef.rmeta: src\lib.rs
+
+c:\Users\nguye\Desktop\Project-Caro\gomoku_ai\target\rls\debug\deps\gomoku_ai-5afd604402168cef.d: src\lib.rs
+
+src\lib.rs:
---
+++ gomoku_ai/target/rls/debug/deps/gomoku_ai-61c48a65a1a4bb4c.d
@@ -0,0 +1,5 @@
+c:\Users\nguye\Desktop\Project-Caro\gomoku_ai\target\rls\debug\deps\gomoku_ai-61c48a65a1a4bb4c.rmeta: src\lib.rs
+
+c:\Users\nguye\Desktop\Project-Caro\gomoku_ai\target\rls\debug\deps\gomoku_ai-61c48a65a1a4bb4c.d: src\lib.rs
+
+src\lib.rs:
