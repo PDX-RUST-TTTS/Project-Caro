65227b981bfaa22863f04b7b4137da930490d357
trilq142 <trilq1402@gmail.com>
2021-06-03 21:51:51 -0700

    Delete gomoku_ai/target directory

--- gomoku_ai/target/rls/debug/deps/test_win-8af8db5107366734.d
+++
@@ -1,5 +0,0 @@
-c:\Users\nguye\Desktop\Project-Caro\gomoku_ai\target\rls\debug\deps\test_win-8af8db5107366734.rmeta: tests\test_win.rs
-
-c:\Users\nguye\Desktop\Project-Caro\gomoku_ai\target\rls\debug\deps\test_win-8af8db5107366734.d: tests\test_win.rs
-
-tests\test_win.rs:
