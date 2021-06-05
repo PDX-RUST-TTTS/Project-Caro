4a59732dbd9394bccf17546b78cf7a6c2582d5ba
anthonylqt24 <tony24@pdx.edu>
2021-06-04 19:18:33 -0700

    add test_ai.rs and some functions to lib.rs
    write some tests for the AI and add functions to the AI crate

--- gomoku_ai/src/lib.rs
+++ gomoku_ai/src/lib.rs
@@ -21,8 +21,6 @@ impl Point {
     }
 }
 
-
-
 #[derive(Default, Debug, Clone)]
 pub struct Player {
     pub side: u8,
@@ -30,10 +28,8 @@ pub struct Player {
     pub total_score: i32,
 }
 
-
-impl Player { 
-
-   pub fn new(side: u8) -> Self {
+impl Player {
+    pub fn new(side: u8) -> Self {
         Self {
             side,
             point_dic: Vec::new(),
@@ -291,6 +287,42 @@ impl Player {
         false
     }
 
+    //Return true -> this player is the winner
+    pub fn add_new_point(
+        &mut self,
+        point: Point,
+        matrix: &mut [[u8; MAX]; MAX],
+        test_point: &mut f32,
+    ) -> bool {
+        let mut yes_test = false;
+        if *test_point > 0.0 {
+            yes_test = true;
+        }
+
+        let origin_test_point = *test_point;
+
+        if self.check_line(&point, matrix, test_point) {
+            if !yes_test {
+                matrix[point.x][point.y] = self.side;
+                self.point_dic.push(point);
+                *test_point = origin_test_point;
+            } else if *test_point == 0.0 {
+                *test_point = origin_test_point;
+            }
+            return true;
+        }
+        if yes_test {
+            if *test_point == 0.0 {
+                *test_point = origin_test_point;
+            }
+        } else {
+            *test_point = origin_test_point;
+            matrix[point.x][point.y] = self.side;
+            self.point_dic.push(point);
+        }
+        false
+    }
+
     fn opposite_side(&self) -> u8 {
         if self.side == 1 {
             2
@@ -298,5 +330,149 @@ impl Player {
             1
         }
     }
+}
 
+pub fn find_best_move(
+    mut ai: Player,
+    mut user: Player,
+    mut matrix: [[u8; MAX]; MAX],
+) -> Option<Point> {
+    let mut hash: HashMap<Point, i32> = HashMap::new();
+    get_all_board_move(&mut hash, ai.point_dic.clone(), &matrix);
+    get_all_board_move(&mut hash, user.point_dic.clone(), &matrix);
+    let new_board = hash.keys().cloned().collect::<Vec<Point>>();
+    let mut final_board: Vec<(Point, f32)> = Vec::new();
+    for point in new_board {
+        let mut test_for_ai = 0.0001;
+        let mut test_for_user = 0.0001;
+
+        if ai.add_new_point(point.clone(), &mut matrix, &mut test_for_ai) {
+            test_for_ai = 1000.0;
+        }
+
+        if user.add_new_point(point.clone(), &mut matrix, &mut test_for_user) {
+            test_for_user = 1000.0;
+        }
+        test_for_ai += 1.0;
+        // if test_for_user > test_for_ai {
+        //     final_board.push((point, test_for_user));
+        // } else {
+        //     final_board.push((point, test_for_ai));
+        // }
+        final_board.push((point, test_for_ai + test_for_user));
+    }
+    final_board.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+    final_board.reverse();
+    println!("###Considering points:");
+    for (point, score) in &final_board {
+        print!("({},{})={} ", point.x, point.y, score);
+    }
+    println!();
+
+    if !final_board.is_empty() {
+        let (result, _) = &final_board[0];
+        return Some(result.clone());
+    }
+    None
+}
+
+pub fn get_all_board_move(
+    result: &mut HashMap<Point, i32>,
+    local_point: Vec<Point>,
+    matrix: &[[u8; MAX]; MAX],
+) {
+    for point in local_point {
+        //Horizon
+        for c in 1..DISTANCE {
+            if point.y + c == MAX {
+                break;
+            }
+            if matrix[point.x][point.y + c] == 0 {
+                result.entry(Point::new(point.x, point.y + c)).or_insert(0);
+            } else {
+                break;
+            }
+        }
+        for c in 1..DISTANCE {
+            if point.y as i16 - c as i16 == -1 {
+                break;
+            }
+            if matrix[point.x][point.y - c] == 0 {
+                result.entry(Point::new(point.x, point.y - c)).or_insert(0);
+            } else {
+                break;
+            }
+        }
+        //Vertical
+        for c in 1..DISTANCE {
+            if point.x + c == MAX {
+                break;
+            }
+            if matrix[point.x + c][point.y] == 0 {
+                result.entry(Point::new(point.x + c, point.y)).or_insert(0);
+            } else {
+                break;
+            }
+        }
+        for c in 1..DISTANCE {
+            if point.x as i16 - c as i16 == -1 {
+                break;
+            }
+            if matrix[point.x - c][point.y] == 0 {
+                result.entry(Point::new(point.x - c, point.y)).or_insert(0);
+            } else {
+                break;
+            }
+        }
+        //Cross down
+        for c in 1..DISTANCE {
+            if point.x + c == MAX || point.y + c == MAX {
+                break;
+            }
+            if matrix[point.x + c][point.y + c] == 0 {
+                result
+                    .entry(Point::new(point.x + c, point.y + c))
+                    .or_insert(0);
+            } else {
+                break;
+            }
+        }
+        for c in 1..DISTANCE {
+            if point.x as i16 - c as i16 == -1 || point.y as i16 - c as i16 == -1 {
+                break;
+            }
+            if matrix[point.x - c][point.y - c] == 0 {
+                result
+                    .entry(Point::new(point.x - c, point.y - c))
+                    .or_insert(0);
+            } else {
+                break;
+            }
+        }
+        //Cross up
+        for c in 1..DISTANCE {
+            if point.x as i16 - c as i16 == -1 || point.y + c == MAX {
+                break;
+            }
+            if matrix[point.x - c][point.y + c] == 0 {
+                result
+                    .entry(Point::new(point.x - c, point.y + c))
+                    .or_insert(0);
+            } else {
+                break;
+            }
+        }
+        for c in 1..DISTANCE {
+            if point.x + c == MAX || point.y as i16 - c as i16 == -1 {
+                break;
+            }
+            if matrix[point.x + c][point.y - c] == 0 {
+                result
+                    .entry(Point::new(point.x + c, point.y - c))
+                    .or_insert(0);
+            } else {
+                break;
+            }
+        }
+    }
 }
